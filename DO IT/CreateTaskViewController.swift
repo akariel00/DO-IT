6a9eecde2af30cbbd7f0734cb1dcb71c81917d21
Ariel Nelson <akariel@gmail.com>
2017-02-01 11:08:10 -0600

    Add stuff…

---
+++ DO IT/CreateTaskViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CreateTaskViewController.swift
+//  DO IT
+//
+//  Created by Ariel Nelson on 2/1/17.
+//  Copyright © 2017 Ariel Nelson. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+        //Create new task from text field outlet
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        //Add task to previeous view controller
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
